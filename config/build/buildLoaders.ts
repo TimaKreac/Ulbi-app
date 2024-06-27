4ef7109dc8e13ab7030285837781c2e7ea59ef32
Tamerlan Telgarin <t.telgarin@qomek.kz>
2024-06-27 23:29:05 +0500

    add config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,34 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types/config';
+
+export const buildLoaders = (options: BuildOptions): webpack.RuleSetRule[] => {
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
+
+  const sassLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: (resPath: string) => resPath.includes('.module.'),
+            localIdentName: options.isDev
+              ? '[hash:base64:8]'
+              : '[path][name]__[local]--[hash:base64:5]',
+          },
+        },
+      },
+      'sass-loader',
+    ],
+  };
+
+  return [typescriptLoader, sassLoader];
+};
